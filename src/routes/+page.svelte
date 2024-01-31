445f80487d5bb5f22ec302550bc0886f0f9a3d69
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-01-31 14:27:51 +0000

    install tailwindCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,12 @@
 <div class="index">
   <p>this is index page from +page</p>
 </div>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
