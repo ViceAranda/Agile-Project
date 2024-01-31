972e2e176c835669d369b7bbcb19c8169f569ebb
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-01-31 15:47:44 +0000

    desktop footer to tailwind css

--- src/components/Footer/DesktopFooter.svelte
+++ src/components/Footer/DesktopFooter.svelte
@@ -1,45 +1,50 @@
-<div class="desktop-footer-container">
-	<div class="desktop-footer logo">
-		<img src="/logo-no-background 2.png" alt="footer logo">
-		<p style="position: fixed; bottom: 0;">Copyright 2023 - Allrights reserved</p>
-	</div>
+<div class="border-t-2 border-black max-h-200 bg-white text-black p-10 fixed bottom-0 w-full box-border flex justify-between">
 
-	<div class="desktop-footer category">
-		<div class="boys-list">
-			<h2><u>Boys</u></h2>
-			<p>Shirts</p>
-			<p>Jackets</p>
-			<p>Trousers</p>
-			<p>Socks</p>
-			<p>Jumpers</p>
-		</div>
-		<div class="girls-list">
-			<h2><u>Girls</u></h2>
-			<p>Shirts</p>
-			<p>Jackets</p>
-			<p>Trousers</p>
-			<p>Socks</p>
-			<p>Jumpers</p>
-		</div>
-		<div class="sports-list">
-			<h2><u>Sports</u></h2>
-			<p>Shirts</p>
-			<p>Jackets</p>
-			<p>Trousers</p>
-			<p>Socks</p>
-			<p>Jumpers</p>
-		</div>
-		<div class="footwear-list">
-			<h2><u>Footwear</u></h2>
-			<p>Shirts</p>
-			<p>Jackets</p>
-			<p>Trousers</p>
-			<p>Socks</p>
-			<p>Jumpers</p>
+	<div class="flex-1 text-center">
+		<div class="bg-white">
+			<img src="/logo-no-background 2.png" alt="footer logo">
+			<p class="text-start">Copyright 2023 - All rights reserved</p>
 		</div>
 	</div>
 
-	<div class="desktop-footer contact">
+	<div class="flex-1 text-center">
+		<div class="bg-white flex justify-evenly">
+			<div class="boys-list">
+				<h2><u>Boys</u></h2>
+				<p>Shirts</p>
+				<p>Jackets</p>
+				<p>Trousers</p>
+				<p>Socks</p>
+				<p>Jumpers</p>
+			</div>
+			<div class="girls-list">
+				<h2><u>Girls</u></h2>
+				<p>Shirts</p>
+				<p>Jackets</p>
+				<p>Trousers</p>
+				<p>Socks</p>
+				<p>Jumpers</p>
+			</div>
+			<div class="sports-list">
+				<h2><u>Sports</u></h2>
+				<p>Shirts</p>
+				<p>Jackets</p>
+				<p>Trousers</p>
+				<p>Socks</p>
+				<p>Jumpers</p>
+			</div>
+			<div class="footwear-list">
+				<h2><u>Footwear</u></h2>
+				<p>Shirts</p>
+				<p>Jackets</p>
+				<p>Trousers</p>
+				<p>Socks</p>
+				<p>Jumpers</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="flex-1 text-right pr-10">
 		<h2><u>CONTACT US</u></h2>
 		<p>123 Fake street</p>
 		<p>London</p>
@@ -47,53 +52,5 @@
 		<p>info@classclothco.com</p>
 		<p>support@classclothco.com</p>
 	</div>
+
 </div>
-
-<style>
-	.desktop-footer-container {
-		border-top: 2px solid #000;
-		max-height: 200px;
-		background-color: #fff;
-		color: #000;
-		padding: 10px;
-		position: fixed;
-		bottom: 0;
-		width:100%;
-		box-sizing: border-box;
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.desktop-footer {
-		flex: 1;
-		text-align: center;
-	}
-
-	.logo {
-		background-color: #fff;
-	}
-
-	.category {
-		background-color: #fff;
-		display: flex;
-		justify-content: space-evenly;
-	}
-
-	.category p {
-		line-height: 20%;
-	}
-
-	.category div {
-		text-align: center;
-	}
-
-	.contact {
-		background-color: #fff;
-	}
-
-	.contact p, h2{
-		line-height: 20%;
-		text-align: end;
-		padding-right: 10%;
-	}
-</style>
