6021459b5fa87d1dc5ba61438e06cec8e211dc76
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-02-04 11:26:53 +0000

    integrate Splide banner

---
+++ src/components/HeroBanner.svelte
@@ -0,0 +1,23 @@
+<!-- integrated HeroBanner by Svelte Splide https://splidejs.com/integration/svelte-splide/ -->
+<script lang="ts">
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    // default theme
+    import '@splidejs/svelte-splide/css';
+</script>
+<Splide
+  options={ {
+    rewind: true,
+    rewindByDrag: true,
+    width: '80%',
+  } }
+>
+    <SplideSlide>
+        <img src="image 1.png" alt="students in uniform" />
+    </SplideSlide>
+    <SplideSlide>
+        <img src="image 1.png" alt="students in uniform" />
+    </SplideSlide>
+    <SplideSlide>
+        <img src="image 1.png" alt="students in uniform" />
+    </SplideSlide>
+</Splide>
