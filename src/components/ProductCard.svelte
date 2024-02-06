dde6e7c4332a26cf3862a14e720cd6ddcce23919
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-02-06 16:43:53 +0000

    add ProductCard

---
+++ src/components/ProductCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let imageUrl:string = '';
+    export let name:string = '';
+    export let price:Number = 0;
+</script>
+
+<div class="relative w-full overflow-hidden rounded-lg">
+<div>
+    <img src={imageUrl} alt="Product Image" class="object-cover rounded-t-lg" />
+</div>
+<div class="p-4 bg-white">
+    <p class="text-xl font-semibold mb-2">{name}</p>
+    <p class="text-lg text-gray-700">£{price.toFixed(2)}</p>
+</div>
+</div>
