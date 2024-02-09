16222081a87bab864c1537d3704ff6f974d9af52
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-02-09 11:03:16 +0000

    fix: resolved abnormal footer order

--- src/lib/components/Footer/DesktopFooter.svelte
+++ src/lib/components/Footer/DesktopFooter.svelte
@@ -1,9 +1,9 @@
 <div class="bg-white text-black p-10 bottom-0 w-full flex flex-col sm:flex-row justify-between mt-4 pl-[5%] pr-[5%]">
 
-	<div class="flex-1 text-center mt-4">
+	<div class="flex-1 text-center mt-4 sm:order-2">
 		<div class="bg-white flex justify-evenly">
 			<div class="boys-list">
-				<h2><u>Boys</u></h2>
+				<h2 class=""><u>Boys</u></h2>
 				<p>Shirts</p>
 				<p>Jackets</p>
 				<p>Trousers</p>
@@ -37,7 +37,7 @@
 		</div>
 	</div>
 
-	<div class="flex-1 text-center mt-4">
+	<div class="flex-1 text-center mt-4 sm:order-3">
 		<h2><u>CONTACT US</u></h2>
 		<p>123 Fake street</p>
 		<p>London</p>
@@ -46,16 +46,16 @@
 		<p>support@classclothco.com</p>
 	</div>
 
-	<div class="flex-1">
+	<div class="flex-1 sm:order-1">
 		<div class="bg-whit">
 			<img 
 			src="/logo-no-background 2.png" 
 			alt="footer logo"
-			class="relative sm:left-[50%] w-40 h-20 sm:bottom-0 sm:mb-4 sm:ml-[40%] sm:transform sm:-translate-x-1/2 sm:translate-y-1"
+			class="relative mx-auto w-40 h-20 sm:bottom-0 sm:mb-4 sm:ml-[40%] sm:transform sm:-translate-x-1/2 sm:translate-y-1"
 			>
 			
 			<p 
-			class="text-end"
+			class="text-center"
 			>
 			Copyright 2023 - All rights reserved
 			</p>
