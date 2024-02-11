25f7ceca136091c20469238f2fc98719839b44c3
Vicente Aranda <vicente.aranda@protonmail.com>
2024-02-10 22:20:47 -0300

    📦 feat: Added API endpoints (products & cetagories)

--- src/lib/server/db/products.ts
+++ src/lib/server/db/products.ts
@@ -11,5 +11,25 @@ export async function selectAll() {
 
     const sql = 'SELECT * FROM PRODUCTS';
     const [ rows ] = await conn.query(sql);
+
+    conn.destroy();
+    return rows;
+}
+
+/**
+ * Might return errors
+ * @param category 
+ * @returns List of Products
+ */
+export async function selectByCategory(category: string) {
+    const conn = await initConnection();
+    conn.connect();
+
+    const sql = `SELECT PRODUCTS.* FROM PRODUCTS 
+    INNER JOIN CATEGORIES ON PRODUCTS.category_id = CATEGORIES.id 
+    WHERE CATEGORIES.name = ` + conn.escape(category);
+
+    const [ rows ] = await conn.query(sql);
+    conn.destroy();
     return rows;
 }
