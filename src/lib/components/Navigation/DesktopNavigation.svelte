1fda0305d70e5a47780f46fbb9c2dd47200f5eed
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-02-16 12:44:26 +0000

    add profile view

--- src/lib/components/Navigation/DesktopNavigation.svelte
+++ src/lib/components/Navigation/DesktopNavigation.svelte
@@ -1,6 +1,7 @@
 <script  lang="ts">
 	let pages: string[] = ['Home', 'Boys', 'Girls', 'Sports', 'Footwear'];
 	let activePage: string = 'Home';
+	let profilePath: string = '/profile' ;
 	function setActivePage(page: string) {
 		activePage = page;
 	}
@@ -112,7 +113,7 @@
      
 		
 		<div class="space-y-7">
-			<button class="bg-white text-black rounded-md py-3 px-4 hover:bg-gray-200 hover:text-black items-center justify-centertext-center" on:click={() => showModal = true}>
+			<button class="bg-white text-black rounded-md py-3 px-4 hover:bg-gray-200 hover:text-black items-center justify-centertext-center" on:click={() => window.location.href = profilePath}>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
 					<path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd"></path>
 				</svg>
