7e75a33074e6b2e12bc84fb274b5401dc57c2887
Vicente Aranda <vicente.aranda@protonmail.com>
2024-02-17 20:37:20 -0300

    📦 feat: Added  remaining product api searches.
    - Added search by school id.
    - Added search by search term (fuzzy).

--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import { selectAll, selectByCategory, selectById } from '$lib/server/db/products';
+import { fuzzySelect, selectAll, selectByCategory, selectById, selectBySchool } from '$lib/server/db/products';
 
 /**
  * 
@@ -7,25 +7,41 @@ import { selectAll, selectByCategory, selectById } from '$lib/server/db/products
  * - Select All
  * - Select Single Product
  * - Select By Category
- * - Select By School [PENDING]
+ * - Select By School
+ * - Select By Search term.
+ * 
+ * Searching by a term performs fuzzy search on the database based on category names, and product names.
  * 
  * Use it as:
  * - `fetch('/api/products')`
  * - `fetch('/api/products?id=123')`
  * - `fetch('/api/products?category=Shoewear')`
+ * - `fetch('/api/products?school_id=123')`
+ * - `fetch('/api/products?search_term=123')`
+ * 
+ * Search is conducted by preference as:
+ * 1. Product id
+ * 2. Category name
+ * 3. School id
+ * 4. Fuzzy term
+ * 5. Default general search
 */
 export async function GET({ url }) {
     console.log('Request data: ', url);
     const productId = url.searchParams.get('id');
     const category = url.searchParams.get('category');
+    const schoolId = url.searchParams.get('school_id');
+    const searchTerm = url.searchParams.get('search_term');
 
     try {
         let results;
-        if (productId) results = await selectById(parseInt(productId));
+
+        if (productId) results = await selectById(productId);
         else if (category) results = await selectByCategory(category);
+        else if (schoolId) results = await selectBySchool(schoolId);
+        else if (searchTerm) results = await fuzzySelect(searchTerm);
         else results = await selectAll();
         
-        console.log(results);
         return json(results);
     } catch (error) {
         // Error log for devs
