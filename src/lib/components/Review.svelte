ea0add359a8aef36a572f00d339cc35f42430bff
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-02-18 11:54:21 +0000

    add review component

---
+++ src/lib/components/Review.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let customerName = "James";
+    export let productName = "Uniform";
+    export let score = 1;
+    export let comments = "";
+</script>
+  
+<div class="customer-review border p-4 m-4 rounded-lg shadow-md">
+    <h3 class="text-xl font-semibold mb-2 text-gray-800">{customerName}'s Review</h3>
+    <p>Product: {productName}</p>
+    <p>Score: {score} </p>
+    <p>Comments: {comments}</p>
+</div>
+
