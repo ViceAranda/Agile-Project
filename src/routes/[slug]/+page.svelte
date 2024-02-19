e4b7321a7155ebc6944d57808be276a95713e16c
chrismcly <93822570+chrismcly@users.noreply.github.com>
2024-02-19 11:41:10 +0000

    create dynamic route

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import HeroBanner from "$lib/components/HeroBanner.svelte";
+    import Review from "$lib/components/Review.svelte";
+</script>
+<div class="flex justify-center">
+    <div class="left-container w-[50%]">
+        <div class="carousel bg-yellow-200">
+            <HeroBanner/>
+        </div>
+        <div class="review bg-orange-500">
+            <Review />
+        </div>
+    </div>
+    <div class="right-container bg-blue-500">
+        <div>this is product description</div>
+    </div>
+</div>
