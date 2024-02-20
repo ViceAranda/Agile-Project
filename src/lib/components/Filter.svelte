f7ff988b540845458861e7784847ae88fbddb38a
FHasaj <products@fhasaj.co.uk>
2024-02-20 16:32:34 +0000

    Filtering working can filter based on params as well as on nav button

--- src/lib/components/Filter.svelte
+++ src/lib/components/Filter.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
-
-import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher();
   
-  // enum variables need to later match database
   enum Price {
-    all = 'all',
-    low = '10',
-    mid = '15-20',
-    high = '21+',
+    low = '14',
+    mid = '15-24',
+    high = '25+',
   }
 
   enum Size {
-    s = 'Age 5-6',
-    m = 'Age 7-8',
-    l = 'Age 9-12',
-    xl = 'Age 13-16',
+    s = '1-6',
+    m = '7-8',
+    l = '9-12',
+    xl = '13-16',
   }
 
   enum Fit {
@@ -24,64 +21,72 @@ import { createEventDispatcher } from 'svelte';
     tailed = 'Tailed Fit',
   }
 
-  let selectedPrice: Price = Price.all;
-  let selectedSize: Size = Size.s;
-  let selectedFit: Fit = Fit.standard;
+  let selectedPrice: any = [];
+  let selectedSize: any= [];
+  let selectedFit: any = [];
 
   $: dispatch('filterChange', { selectedPrice, selectedSize, selectedFit });
-
-  //functions to apply filter base on selected option
-  //function filterSelectedPrice etc.
+  
 </script>
 
-<aside class="flex justify-center">
+<aside class="flex justify-left w-full pl-[20%]">
   <div class="filters-container">
     <div class="price-container py-4 border-b-2 border-solid border-black">
       <h2 class=" text-xl underline pb-3">Price</h2>
       <label>
-      <input type="radio" bind:group={selectedPrice} value={Price.all} /> {'All'}
+      <input type="checkbox" bind:group={selectedPrice} value={Price.low} /> {Price.low}
       </label> <br>
       <label>
-      <input type="radio" bind:group={selectedPrice} value={Price.low} /> {'< £10'}
-      </label> <br>
-      <label>
-      <input type="radio" bind:group={selectedPrice} value={Price.mid} /> {'£15-20'}
+      <input type="checkbox" bind:group={selectedPrice} value={Price.mid} /> {Price.mid}
       </label> <br>
       <label> 
-      <input type="radio" bind:group={selectedPrice} value={Price.high} /> {' £21+'}
+      <input type="checkbox" bind:group={selectedPrice} value={Price.high} /> {Price.high} 
       </label>
-      <p>selected price group is {selectedPrice}</p>
     </div>
 
     <div class="size-container py-4 border-b-2 border-solid border-black">
       <h2 class=" text-xl underline pb-3">Size</h2>
       <label>
-      <input type="radio" bind:group={selectedSize} value={Size.s} /> {Size.s}
+      <input type="checkbox" bind:group={selectedSize} value={Size.s} /> {Size.s}
       </label> <br>
       <label>
-      <input type="radio" bind:group={selectedSize} value={Size.m} /> {Size.m}
+      <input type="checkbox" bind:group={selectedSize} value={Size.m} /> {Size.m}
       </label> <br>
       <label>
-      <input type="radio" bind:group={selectedSize} value={Size.l} /> {Size.l}
+      <input type="checkbox" bind:group={selectedSize} value={Size.l} /> {Size.l}
       </label> <br>
       <label>
-      <input type="radio" bind:group={selectedSize} value={Size.xl} /> {Size.xl}
+      <input type="checkbox" bind:group={selectedSize} value={Size.xl} /> {Size.xl}
       </label>
-      <p>selected Size group is {selectedSize}</p>
     </div>
 
-    <div class="fit-container py-4 border-b-2 border-solid border-black">
-      <h2 class=" text-xl underline">Fit</h2>
+    <div class="fit-container py-4 border-b-2 border-solid border-black mb-2">
+      <h2 class=" text-xl underline mb-2">Fit</h2>
       <label>
-      <input type="radio" bind:group={selectedFit} value={Fit.standard} /> {Fit.standard}
+      <input type="checkbox" bind:group={selectedFit} value={Fit.standard} /> {Fit.standard}
       </label> <br>
       <label>
-      <input type="radio" bind:group={selectedFit} value={Fit.slim} /> {Fit.slim}
+      <input type="checkbox" bind:group={selectedFit} value={Fit.slim} /> {Fit.slim}
       </label> <br>
       <label>
-      <input type="radio" bind:group={selectedFit} value={Fit.tailed} /> {Fit.tailed}
+      <input type="checkbox" bind:group={selectedFit} value={Fit.tailed} /> {Fit.tailed}
       </label> <br>
-      <p>selected Fit group is {selectedFit}</p>
     </div>
+
+    <div class='mt-5'>
+      <button 
+      class="bg-black text-white p-2 rounded-md disabled:bg-gray-300 disabled:opacity-0" 
+      on:click={() => {
+        selectedPrice = [];
+        selectedSize = [];
+        selectedFit = [];
+      }}
+      disabled={selectedPrice.length === 0 && selectedSize.length === 0 && selectedFit.length === 0}
+      >
+
+      Clear Filters
+      </button>
+    </div>
+
   </div>
 </aside>
